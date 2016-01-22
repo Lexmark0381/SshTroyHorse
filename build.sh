2694a575df0c3dbe2509e522e6badf2c36d0adf6
Piermarco Barbè <Lexmark0381@users.noreply.github.com>
2016-01-22 01:55:21 +0100

    First Commits

---
+++ build.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+clear
+name="$(pwd)"
+size=${#name}
+name=${name:9:size}
+if [ ! $name == "keyInstaller" ];
+	then
+	echo "Renaming USB Key..."
+	/usr/sbin/diskutil rename $name keyInstaller
+	cd ..
+	cd keyInstaller
+	echo "Done!"
+	else
+	echo "USB Name is correct                    ✓"
+fi
+if [ ! -f id_rsa.pub ];
+	then
+	cp ~/.ssh/id_rsa.pub . && echo "Public Key Copied!"
+	else
+	echo "id_rsa.pub exists                      ✓"
+fi
+if [ ! -f whoamis.txt ];
+	then
+	touch whoamis.txt && echo "whoamis.txt created!"
+	else
+	echo "whoamis.txt exists                     ✓"
+fi
+if [ ! -f hostnames.txt ];
+	then
+	touch hostnames.txt && echo "hostnames.txt created!"
+	else
+	echo "hostnames.txt exists                   ✓"
+fi
+chmod +x script.sh && echo "script.sh is now executable            ✓"
+
+echo "     _                  " 
+echo "  __| | ___  _ __   ___ " 
+echo " / _  |/ _ \|  _ \ / _ \ "
+echo "| (_| | (_) | | | |  __/ "
+echo " \__,_|\___/|_| |_|\___| "
+
+
---
+++ script.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+open /System/Library/PreferencePanes/SharingPref.prefPane/ && cat /Volumes/keyInstaller/id_rsa.pub >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && whoami >> /Volumes/keyInstaller/whoamis.txt && hostname >> /Volumes/keyInstaller/hostnames.txt && toeject=$(df -h | grep /Volumes/keyInstaller) && toeject=${toeject:0:12}diskutil unmount 
+diskutil unmountDisk force 
